6c68e7c23e75bf117b0ad8c280d6cc6380a169c2
PRAJWAL.C <88227526+prajwalcpraju@users.noreply.github.com>
2021-12-25 16:42:43 +0530

    Add files via upload

---
+++ project/project.py
@@ -0,0 +1,62 @@
+groups=[grp1,grp2,grp3,grp4,grp5]
+grp_name=['group1','group2','group3','group4','group5']
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+tk_words=[]
+for i,items in enumerate(groups):
+    t=word_tokenize(groups[i])
+    tk_words.append(t)
+    
+fw_words=[]
+for i,items in enumerate(tk_words):
+    sw=stopwords.words('english')
+    final_words=[word for word in items if word not in sw]
+    final_words.sort()
+    fw_words.append(final_words)
+    
+final_groups=list()
+for i,items in enumerate(fw_words):
+    fg= list()
+    for item in fw_words[i]:
+        if item not in fg:
+            fg.append(item)
+    final_groups.append(fg)
+
+with open('sample.txt','r') as file:
+        file_1=file.readlines()
+tk_file= [phrase.split() for phrase in file_1]
+sw=stopwords.words('english')
+final_words=[word for word in tk_file[0] if word not in sw]
+final_words.sort()
+
+sample_file=list()
+for item in final_words:
+    if item not in sample_file:
+        sample_file.append(item)
+
+rating=[]
+for i,items in enumerate(final_groups):
+    a = set(final_groups[i])
+    b = set(sample_file)
+    similarity = float(len(a.intersection(b))*100)
+    rating.append(similarity)
+
+avg=(max(rating)-min(rating))/2
+for i in range(5):
+    if(rating[i]>avg):
+        print("Rating of "+grp_name[i]+" is : 3")
+    else :
+        if(rating[i]<avg and rating[i]>min(rating)):
+            print("Rating of "+grp_name[i]+" is : 2")
+        else:
+            print("Rating of "+grp_name[i]+" is : 1")
+            
+output is :
+Rating of group1 is : 3
+Rating of group2 is : 2
+Rating of group3 is : 1
+Rating of group4 is : 3
+Rating of group5 is : 1
